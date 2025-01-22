d232184e4902273bcdf1ab06b97274faab963690
Krystian <krystian.wachnicki5@gmail.com>
2025-01-22 10:24:34 +0700

    fix: increase request limit for tests

--- packages/server/libs/rate-limit.js
+++ packages/server/libs/rate-limit.js
@@ -2,7 +2,7 @@ import { rateLimit } from "express-rate-limit";
 
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000,
-  limit: 10,
+  limit: process.env.NODE_ENV === "test" ? 100 : 10,
   standardHeaders: "draft-8",
   legacyHeaders: false,
   handler: function (_, res) {
--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import { defineConfig } from "vitest/config";
 
 if (process.env.DATABASE_CLIENT === "sqlite") {
+  process.env.NODE_ENV = "test";
   process.env.DATABASE_PATH = ".db-test/data.sqlite3";
 }
 
