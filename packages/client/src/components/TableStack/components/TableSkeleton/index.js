ff13893286895bbc6597e24006fd9e716fb7d33e
Krystian <krystian.wachnicki5@gmail.com>
2025-02-14 09:06:03 +0700

    fix: remove timeout from table skeleton

--- packages/client/src/components/TableStack/components/TableSkeleton/index.js
+++ packages/client/src/components/TableStack/components/TableSkeleton/index.js
@@ -1,21 +1,8 @@
-import { useEffect, useState } from "react";
 import "./styles.scss";
 
 const mainClass = "table-skeleton";
 
 const TableSkeleton = () => {
-  const [visible, setVisible] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setVisible(true);
-    }, 200);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (!!!visible) return null;
-
   return (
     <div className={mainClass}>
       <div className={`${mainClass}__row`}>
@@ -24,9 +11,6 @@ const TableSkeleton = () => {
       <div className={`${mainClass}__row`}>
         <div className={`skeleton`}></div>
       </div>
-      {/* <div className={`${mainClass}__row`}>
-        <div className={`skeleton`}></div>
-      </div> */}
     </div>
   );
 };
