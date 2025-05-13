2893901604472b7c929d2256da85ae9d0a50b148
Krystian <krystian.wachnicki5@gmail.com>
2025-05-13 21:18:52 +0700

    chore: clean up

--- packages/core/emails/server/api/index.js
+++ packages/core/emails/server/api/index.js
@@ -1,9 +1,7 @@
 import addEmail from "./addEmail.js";
 import getEmails from "./getEmails.js";
 import deleteEmail from "./deleteEmail.js";
-// import deleteMedia from "./deleteMedia.js";
-// import getMediaSettings from "./getMediaSettings.js";
-// import updateMediaSettings from "./updateMediaSettings.js";
+
 import authMiddleware from "../../../auth/utils/authMiddleware.js";
 
 export default [
@@ -25,22 +23,4 @@ export default [
     handler: deleteEmail,
     middlewares: [authMiddleware(["admin"])],
   },
-  // {
-  //   method: "DELETE",
-  //   path: "/media/:id",
-  //   handler: deleteMedia,
-  //   middlewares: [authMiddleware(["admin"])],
-  // },
-  // {
-  //   method: "PUT",
-  //   path: "/media/settings",
-  //   handler: updateMediaSettings,
-  //   middlewares: [authMiddleware(["admin"])],
-  // },
-  // {
-  //   method: "GET",
-  //   path: "/media/settings",
-  //   handler: getMediaSettings,
-  //   middlewares: [authMiddleware(["admin"])],
-  // },
 ];
