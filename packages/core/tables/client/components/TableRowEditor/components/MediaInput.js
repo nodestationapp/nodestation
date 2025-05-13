129159084b4fbde91b5fa06b3044c4b391f8566e
Krystian <krystian.wachnicki5@gmail.com>
2025-05-13 08:54:08 +0700

    feat: add media input

--- packages/core/tables/client/components/TableRowEditor/components/MediaInput.js
+++ packages/core/tables/client/components/TableRowEditor/components/MediaInput.js
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import MediaDialog from "../../../../../media/client/components/MediaDialog/index.js";
 
+import {
+  Stack,
+  Select,
+  Avatar,
+  Typography,
+  InputLabel,
+  FormControl,
+} from "@mui/material";
+
 const MediaInput = ({ value, onChange }) => {
   const [uploadDialogOpen, setUploadDialogOpen] = useState(false);
 
@@ -11,9 +20,31 @@ const MediaInput = ({ value, onChange }) => {
 
   return (
     <>
-      <button onClick={() => setUploadDialogOpen(true)}>
-        {!!value ? value?.name : "Open file"}
-      </button>
+      <FormControl fullWidth size="large">
+        <InputLabel id="photo-label">Photo</InputLabel>
+        <Select
+          fullWidth
+          open={false}
+          label="Photo"
+          onClick={(e) => {
+            e.preventDefault();
+            setUploadDialogOpen(true);
+          }}
+          labelId="photo-label"
+          value={true}
+          renderValue={() => (
+            <Stack direction="row" alignItems="center" gap={1}>
+              <Avatar
+                src={value?.url}
+                sx={{ width: 34, height: 34, objectFit: "cover" }}
+              />
+              <Typography color={!!value ? "text.primary" : "text.secondary"}>
+                {value?.name || "Empty"}
+              </Typography>
+            </Stack>
+          )}
+        />
+      </FormControl>
       <MediaDialog
         value={value}
         open={uploadDialogOpen}
