d9394cf2af040837360373f29aa79c5385bec767
Krystian <krystian.wachnicki5@gmail.com>
2025-05-26 18:54:56 +0700

    fix: add parser in create route

--- packages/nodestation/utils/loadRoute.js
+++ packages/nodestation/utils/loadRoute.js
@@ -4,7 +4,7 @@ const loadRoute = async (router, routes) => {
   for await (const route of routes) {
     router[route?.method.toLowerCase()](
       `/api${route?.path}`,
-      route?.properties?.parser === "raw"
+      route?.parser === "raw"
         ? express.raw({ type: "application/json" })
         : express.json(),
       ...(route?.middlewares || []),
